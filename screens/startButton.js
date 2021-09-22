c20e8636369c2518cc37ee70cd224f48a3457dd7
Gordon Moore <gmgit@gordonmoore.co.uk>
2021-09-22 13:02:54 +0100

    Can move start button around by gesture and panhandler - not needed, but wanted to see how it was done. Will revert back to animated onPress

--- screens/startButton.js
+++ screens/startButton.js
@@ -11,7 +11,7 @@
  */
 
 import React, { useState, useContext, useEffect, useRef } from 'react'
-import {Animated, View, TouchableOpacity, Text} from 'react-native'
+import {Animated, PanResponder, View, TouchableOpacity, Text} from 'react-native'
 import { global } from '../styles/global'
 
 import Context from '../context/Context'
@@ -24,8 +24,32 @@ const StartButton = () => {
   const [pos, setPos] = useState({ x: 100, y: 400 })
 
   let position
-  position = useRef(new Animated.ValueXY(pos)).current  
+  // position = useRef(new Animated.ValueXY(pos)).current  
+  position = useRef(new Animated.ValueXY()).current  
   //console.log(position)
+
+  const panResponder = useRef(
+    PanResponder.create({
+
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+
+      onPanResponderGrant: (evt, gestureState) => {
+        position.setOffset({
+          x: position.x._value,
+          y: position.y._value
+        })
+      },
+      onPanResponderMove: Animated.event(
+        [
+          null,
+          { dx: position.x, dy: position.y }
+        ], {useNativeDriver: false}
+      ),
+      onPanResponderRelease: (evt, gestureState) => {}
+    })
+  ).current
+  
+
 
   useEffect( () => {
 
@@ -34,34 +58,44 @@ const StartButton = () => {
   const starting = () => {
     console.log('starting...')
 
-    if (x) {    
-      debugger
-      setX(false)
-      setPos( { x: 100, y: 400 } )
-      console.log(`true ${position}`)
-    }
-    else {
-      setX(true)
-      setPos( { x: 10, y: 10 })
-      console.log(`false ${position}`)
-    }
+    // if (x) {    
+    //   debugger
+    //   setX(false)
+    //   setPos( { x: 100, y: 400 } )
+    //   console.log(`true ${position}`)
+    // }
+    // else {
+    //   setX(true)
+    //   setPos( { x: 10, y: 10 })
+    //   console.log(`false ${position}`)
+    // }
   }
 
   useEffect( () => {  //because setting state is asynchronous
-    Animated.spring(position, {
-      toValue: pos,
-      useNativeDriver: false,
-    }).start()
+    // Animated.spring(position, {
+    //   toValue: pos,
+    //   useNativeDriver: false,
+    // }).start()
   }, [pos])
 
   return (
-    <Animated.View style={position.getLayout()}>
-      <TouchableOpacity style={global.startButton2} onPress={starting}>
+
+
+    <Animated.View style={{
+         transform: [{translateX: position.x}, { translateY: position.y}] 
+       }}  
+       {...panResponder.panHandlers} >
+      <TouchableOpacity style={global.startButton2}>
       {/* <TouchableOpacity  onPress={starting}> */}
         <Text style={global.startButtonText}>Start</Text>
       </TouchableOpacity>
     </Animated.View>
+
   )
 }
 
 export default StartButton
+    // <Animated.View >
+ // < TouchableOpacity style = { global.startButton2 } onPress = { starting } >
+// <Animated.View style={position.getLayout()}>
+    // </Animated.View>
